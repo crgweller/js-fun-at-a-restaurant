081cfecddbad8e2a8b89e386ac4823d321cf58b8
Craig Weller <crg.weller@gmail.com>
2022-10-15 08:49:19 -0600

    Update finished order.js project

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(orders, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+  deliveryOrders.push(orders)
+  return orders
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if (orderNum === deliveryOrders[i].orderNumber) {
+    deliveryOrders.splice(i, 1)
+  }
+    }
+      }
+
+function listItems(deliveryOrders) {
+  var sandwiches = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+sandwiches.push(deliveryOrders[i].item)
+}
+return sandwiches.join(`, `)
+    }
+
+function searchOrder(orders, item) {
+  var found = false
+for (var i = 0; i < orders.length; i++) {
+  if (orders[i].item.includes(item)) {
+  found = true
+  }
+}
+console.log(found)
+return found
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
